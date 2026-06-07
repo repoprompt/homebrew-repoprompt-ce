f67d42a55cbe734ded9fb5b1c41e56f878a2ce35
Najib Ninaba <najibninaba@gmail.com>
2026-06-07 15:14:20 +0800

    Bump RepoPrompt CE cask to 1.0.8

--- Casks/repoprompt-ce.rb
+++ Casks/repoprompt-ce.rb
@@ -1,6 +1,6 @@
 cask "repoprompt-ce" do
-  version "1.0.7,8"
-  sha256 "f36ec61cc714b8190011f800c631102f12ed3f4e61217ace1dad6a15a8d60590"
+  version "1.0.8,9"
+  sha256 "20ddcd94e2d95032a34b859560605f7f36b606ca16ca78cec1a01defd9b7bb3c"
 
   url "https://github.com/repoprompt/repoprompt-ce-updates/releases/download/v#{version.csv.first}/RepoPrompt-#{version.csv.first}-#{version.csv.second}.zip"
   name "RepoPrompt CE"
