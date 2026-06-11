0ca49b5b288fdfd1c1ae9139d41b08e76771d812
SB <richstyles@gmail.com>
2026-06-11 18:01:26 +0900

    Bump RepoPrompt CE cask to 1.0.14

--- Casks/repoprompt-ce.rb
+++ Casks/repoprompt-ce.rb
@@ -1,6 +1,6 @@
 cask "repoprompt-ce" do
-  version "1.0.13,14"
-  sha256 "51a7c34fb6b3c0e72fa4a9672bbc519676631a0eb328c43de1a61aaf09b56839"
+  version "1.0.14,15"
+  sha256 "f45ed69094c06ac18e4ee7318125895c341f41ac93f7f31092ad0268ba0ce5e2"
 
   url "https://github.com/repoprompt/repoprompt-ce-updates/releases/download/v#{version.csv.first}/RepoPrompt-#{version.csv.first}-#{version.csv.second}.zip"
   name "RepoPrompt CE"
