cb3d66b6f22d69246b01eb9e7079aa2f47985614
Najib Ninaba <najibninaba@gmail.com>
2026-07-30 07:28:37 +0800

    Update RepoPrompt CE to 1.1.1

--- Casks/repoprompt-ce.rb
+++ Casks/repoprompt-ce.rb
@@ -1,6 +1,6 @@
 cask "repoprompt-ce" do
-  version "1.1.0,31"
-  sha256 "d55a2f5564d917311799618b119cc0a2ed93fbcae2d1c0ee5fb0508c762b6cd1"
+  version "1.1.1,32"
+  sha256 "9b3c697e7a63a0e1ca2b2c7497bcec59e66c5e671843e8d1b5d4ad8213c67335"
 
   url "https://github.com/repoprompt/repoprompt-ce-updates/releases/download/v#{version.csv.first}/RepoPrompt-#{version.csv.first}-#{version.csv.second}.zip"
   name "RepoPrompt CE"
